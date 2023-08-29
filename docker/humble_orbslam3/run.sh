1304ef26798b5bfd6fd5594b85580dadcfb00fb9
assume <assume0701@gmail.com>
2023-08-29 21:50:53 +0800

    Add a simple script for building and running

---
+++ docker/humble_orbslam3/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DOCKERFILE=Dockerfile
+IMAGE_NAME=orbslam/humble
+CONTAINER_NAME=test
+
+# remove old containers
+docker ps -a | grep $IMAGE_NAME | awk '{print $1}' | xargs -r docker rm
+
+# remove old images
+docker rmi $IMAGE_NAME
+
+# build docker
+docker build -t $IMAGE_NAME -f $DOCKERFILE .
+
+# run docker
+docker run \
+    -it \
+    --rm \
+    --gpus all \
+    -h $(hostname) $2 \
+    --network host \
+    -e DISPLAY=$DISPLAY \
+    -e XAUTHORITY=/tmp/xauth \
+    -v ~/.Xauthority:/tmp/xauth \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --name $CONTAINER_NAME \
+    $IMAGE_NAME
