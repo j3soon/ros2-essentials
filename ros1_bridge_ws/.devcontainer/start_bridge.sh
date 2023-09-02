8bf8682192a77726d4f864d691885c8c06c49eeb
YuZhong-Chen <yuzhong1214@gmail.com>
2023-09-02 12:10:17 +0800

    Reduce image size from 16.5GB to 2.5GB.
    To reduce the image size, I adopted the Multi-stage builds approach, splitting the process into two stages: the build stage and the release stage. In the build stage, I followed the same procedure as before, installing all necessary packages and performing the compilation. The release stage, the critical component, involves solely copying the 'install/' folder from the build stage. This streamlined approach is possible because ROS2 only requires the 'install/' folder for execution.
    
    It's worth noting that the build process is time-consuming. I highly recommend avoiding re-building the image and refraining from modifying the Dockerfile unless you encounter a bug. I've already uploaded the image to Dockerhub, so downloading it would be the most efficient option.
    
    Reference
    - https://docs.docker.com/build/building/multi-stage/

--- ros1_bridge_ws/.devcontainer/start_bridge.sh
+++ ros1_bridge_ws/.devcontainer/start_bridge.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 if [ $# = 1 ]; then
-    ROS_MASTER_URI=$1
+    export ROS_MASTER_URI=$1
 else
-    ROS_MASTER_URI=http://localhost:11311
+    export ROS_MASTER_URI=http://localhost:11311
 fi
 
-source ~/ros2_humble/install/setup.bash
-source ~/ros2_humble/install/ros1_bridge/share/ros1_bridge/local_setup.bash
+source /ros2_humble/install/setup.bash
+source /ros2_humble/install/ros1_bridge/share/ros1_bridge/local_setup.bash
 
-export ROS_MASTER_URI=$ROS_MASTER_URI
 ros2 run ros1_bridge dynamic_bridge
