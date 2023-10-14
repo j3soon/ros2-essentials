ce8773cf23e401556050eaf11a2aaa8a66914b43
YuZhong-Chen <yuzhong1214@gmail.com>
2023-10-14 16:11:50 +0800

    Remove unused imports and change single quotes to double quotes in husky_viz.
    The change in this commit is not necessary, but it will improve code readability.

--- husky_ws/src/husky/husky_viz/launch/view_model_launch.py
+++ husky_ws/src/husky/husky_viz/launch/view_model_launch.py
@@ -1,10 +1,9 @@
 import os
-import sys
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
@@ -17,23 +16,22 @@ def generate_launch_description():
     )
 
     node_joint_state_publisher_gui = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui'
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui"
     )
 
     node_rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz',
-        arguments=['-d', rviz_config_file],
-        output='screen'
+        package="rviz2",
+        executable="rviz2",
+        name="rviz",
+        arguments=["-d", rviz_config_file],
+        output="screen"
     )
 
     launch_husky_description = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory(
-                'husky_description'), 'launch/description_launch.py')
+            os.path.join(get_package_share_directory("husky_description"), "launch/description_launch.py")
         )
     )
 
--- husky_ws/src/husky/husky_viz/launch/view_robot_launch.py
+++ husky_ws/src/husky/husky_viz/launch/view_robot_launch.py
@@ -1,13 +1,7 @@
-import os
-import sys
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
@@ -17,11 +11,11 @@ def generate_launch_description():
     )
 
     node_rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz',
-        arguments=['-d', rviz_config_file],
-        output='screen'
+        package="rviz2",
+        executable="rviz2",
+        name="rviz",
+        arguments=["-d", rviz_config_file],
+        output="screen"
     )
 
     return LaunchDescription(
