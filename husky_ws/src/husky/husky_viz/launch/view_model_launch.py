57db5879a58fb2fd3cb99d0320ca3cd37c7a1630
YuZhong-Chen <yuzhong1214@gmail.com>
2023-10-14 16:31:03 +0800

    Add comments in husky_viz.

--- husky_ws/src/husky/husky_viz/launch/view_model_launch.py
+++ husky_ws/src/husky/husky_viz/launch/view_model_launch.py
@@ -11,16 +11,19 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
+    # The path of .rviz file.
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare("husky_viz"), "rviz", "test.rviz"]
     )
 
+    # Launch joint state publisher.
     node_joint_state_publisher_gui = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
         name="joint_state_publisher_gui"
     )
 
+    # Launch rviz2.
     node_rviz = Node(
         package="rviz2",
         executable="rviz2",
@@ -29,6 +32,8 @@ def generate_launch_description():
         output="screen"
     )
 
+    # Launch Husky's description. ( Use the launch file in "husky_description" )
+    # It retrieves Husky's URDF via xacro and publishes the robot state.
     launch_husky_description = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory("husky_description"), "launch/description_launch.py")
--- husky_ws/src/husky/husky_viz/launch/view_robot_launch.py
+++ husky_ws/src/husky/husky_viz/launch/view_robot_launch.py
@@ -4,12 +4,16 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
+# This launch file will only launch rviz2 node.
+# It is used when other repos want to launch rviz2 for Husky. For instance: husky_navigation
 def generate_launch_description():
 
+    # The path of .rviz file.
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare("husky_viz"), "rviz", "test.rviz"]
     )
 
+    # Launch rviz2.
     node_rviz = Node(
         package="rviz2",
         executable="rviz2",
