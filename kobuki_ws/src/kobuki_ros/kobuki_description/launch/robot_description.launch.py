a2e16d050d82bcdc9821364779177ed39d9de4c6
YuZhong-Chen <yuzhong1214@gmail.com>
2023-11-22 01:53:54 +0800

    Create kobuki_rviz package.
    Remove the rviz node in kobuki_description and create a separate rviz package for launching rviz.
    If you want to check Kobuki's model or launch rviz in another launch file, please refer to the file `view_model_launch.py`.

--- kobuki_ws/src/kobuki_ros/kobuki_description/launch/robot_description.launch.py
+++ kobuki_ws/src/kobuki_ros/kobuki_description/launch/robot_description.launch.py
@@ -45,10 +45,6 @@ def generate_launch_description():
         [FindPackageShare('kobuki_description'), 'urdf', 'kobuki_standalone.urdf.xacro']
     )
 
-    rviz_config_path = PathJoinSubstitution(
-        [FindPackageShare('kobuki_description'), 'rviz', 'model.rviz']
-    )
-
     return LaunchDescription([
 
         DeclareLaunchArgument(
@@ -61,12 +57,6 @@ def generate_launch_description():
             name='use_sim_time',
             default_value='false',
             description='Use simulation time'
-        ),
-
-        DeclareLaunchArgument(
-            name='rviz',
-            default_value='false',
-            description='Run rviz'
         ),
 
         Node(
@@ -84,18 +74,6 @@ def generate_launch_description():
             package='joint_state_publisher',
             executable='joint_state_publisher',
             name='joint_state_publisher',
-            parameters=[{
-                'use_sim_time': LaunchConfiguration('use_sim_time')
-            }]
-        ),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            arguments=['-d', rviz_config_path],
-            condition=IfCondition(LaunchConfiguration('rviz')),
             parameters=[{
                 'use_sim_time': LaunchConfiguration('use_sim_time')
             }]
