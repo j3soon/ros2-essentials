YuZhong-Chen/ros2-agv-essentials

Add launch_rviz argument to kobuki_gazebo.
This flag is used to determine whether launch rviz2.

kobuki_ws/src/kobuki_gazebo/launch/gazebo.launch.py
@@ -10,6 +10,7 @@ from launch.substitutions import (
     PathJoinSubstitution,
 )
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 from pathlib import Path
@@ -24,6 +25,11 @@ ARGUMENTS = [
         "world_path",
         default_value="",
         description="The world path, by default is empty.world",
+    ),
+    DeclareLaunchArgument(
+        "launch_rviz",
+        default_value="False",
+        description="Launch rviz2, by default is True",
     ),
     DeclareLaunchArgument(
         "GAZEBO_MODEL_PATH",
@@ -64,6 +70,27 @@ def generate_launch_description():
             "world": LaunchConfiguration("world_path"),
         }.items(),
     )
+    
+    # The path of .rviz file.
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare("kobuki_rviz"), "rviz", "gazebo.rviz"]
+    )
+    
+    # Launch rviz2.
+    launch_rviz = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [
+                FindPackageShare("kobuki_rviz"),
+                "launch",
+                "open_rviz_launch.py",
+            ]
+        ),
+        launch_arguments={
+            "use_sim_time": LaunchConfiguration("use_sim_time"),
+            "rviz_config_path": rviz_config_path,
+        }.items(),
+        condition=IfCondition(LaunchConfiguration("launch_rviz")),
+    )
 
     # Launch Kobuki's description.
     launch_kobuki_description = IncludeLaunchDescription(
@@ -93,5 +120,6 @@ def generate_launch_description():
     ld.add_action(launch_gazebo)
     ld.add_action(launch_kobuki_description)
     ld.add_action(spawn_robot)
+    ld.add_action(launch_rviz)
 
     return ld

j3soon/ros2-essentials

kobuki_ws/src/kobuki_gazebo/launch/gazebo.launch.py
@@ -10,6 +10,7 @@ from launch.substitutions import (
     PathJoinSubstitution,
 )
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 from pathlib import Path
@@ -24,6 +25,11 @@ ARGUMENTS = [
         "world_path",
         default_value="",
         description="The world path, by default is empty.world",
+    ),
+    DeclareLaunchArgument(
+        "launch_rviz",
+        default_value="False",
+        description="Launch rviz2, by default is False",
     ),
     DeclareLaunchArgument(
         "GAZEBO_MODEL_PATH",
@@ -64,6 +70,27 @@ def generate_launch_description():
             "world": LaunchConfiguration("world_path"),
         }.items(),
     )
+    
+    # The path of .rviz file.
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare("kobuki_rviz"), "rviz", "gazebo.rviz"]
+    )
+    
+    # Launch rviz2.
+    launch_rviz = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [
+                FindPackageShare("kobuki_rviz"),
+                "launch",
+                "open_rviz_launch.py",
+            ]
+        ),
+        launch_arguments={
+            "use_sim_time": LaunchConfiguration("use_sim_time"),
+            "rviz_config_path": rviz_config_path,
+        }.items(),
+        condition=IfCondition(LaunchConfiguration("launch_rviz")),
+    )
 
     # Launch Kobuki's description.
     launch_kobuki_description = IncludeLaunchDescription(
@@ -93,5 +120,6 @@ def generate_launch_description():
     ld.add_action(launch_gazebo)
     ld.add_action(launch_kobuki_description)
     ld.add_action(spawn_robot)
+    ld.add_action(launch_rviz)
 
     return ld