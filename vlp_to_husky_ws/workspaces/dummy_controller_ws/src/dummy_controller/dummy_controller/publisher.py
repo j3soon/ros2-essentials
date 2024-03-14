a6b002a6bd62d1539205b91b954685a2f9e6928a
assume <assume0701@gmail.com>
2024-03-14 22:50:14 +0800

    Copy husky controller publisher
    Ref: https://github.com/YuZhong-Chen/ros2-agv-essentials/blob/0641a64f0fa454421134e45f6005d9940facf008
    File path: zed_to_husky_ws/workspaces/dummy_controller_ws/src/dummy_controller/dummy_controller/publisher.py

--- vlp_to_husky_ws/workspaces/dummy_controller_ws/src/dummy_controller/dummy_controller/publisher.py
+++ vlp_to_husky_ws/workspaces/dummy_controller_ws/src/dummy_controller/dummy_controller/publisher.py
@@ -15,21 +15,40 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 
-class MinimalPublisher(Node):
+class CmdVelPublisher(Node):
 
-    def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+    def __init__(self, topic_name: str, node_name="cmd_vel_publisher"):
+        super().__init__(node_name)
+        
+        self.cmd_vel_speed = 0.5 # m/s
+        
+        self.publisher_ = self.create_publisher(Twist, topic_name, 10)
+        self.status = "stop"
+        self.msg = Twist()
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    def forward(self, speed):
+        self.status = "forward"
+        self.msg.linear.y = self.msg.linear.z = 0.0
+        self.msg.angular.x = self.msg.angular.y = self.msg.angular.z = 0.0
+        self.msg.linear.x = speed
+
+        self.publish()
+        self.get_logger().info('Publishing speed: %lf' % speed)
+
+    def update_status(self):
+        self.msg.linear.x = self.msg.linear.y = self.msg.linear.z = 0.0
+        self.msg.angular.x = self.msg.angular.y = self.msg.angular.z = 0.0
+        
+        if self.status == "right":
+            self.msg.angular.z = self.cmd_vel_speed
+        elif self.status == "left":
+            self.msg.angular.z = -self.cmd_vel_speed
+        
+        self.publish()
+        self.get_logger().info('Publishing: "%s"' % self.status)
+    
+    def publish(self):
+        self.publisher_.publish(self.msg)
