f10c08aae3716f4f645bed3fab6570c8f6073348
Assume-Zhan <assume0701@gapp.nthu.edu.tw>
2024-08-04 17:21:18 +0800

    feat(vlplidar_ws): Add static tf for lidar and robot frame transformation
    Ref tutorial: https://docs.ros.org/en/humble/Tutorials/Intermediate/Tf2/Writing-A-Tf2-Static-Broadcaster-Py.html#the-proper-way-to-publish-static-transforms

--- vlplidar_ws/src/vlp_cartographer/launch/vlp_driver.launch.py
+++ vlplidar_ws/src/vlp_cartographer/launch/vlp_driver.launch.py
@@ -2,7 +2,7 @@ from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 
 from launch import LaunchDescription
-from launch.conditions import UnlessCondition
+from launch.conditions import UnlessCondition, IfCondition
 from launch.actions import IncludeLaunchDescription, GroupAction, DeclareLaunchArgument
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
@@ -17,6 +17,27 @@ def generate_launch_description():
 
     is_sim = LaunchConfiguration('is_sim')
     is_sim_arg = DeclareLaunchArgument('is_sim', default_value='False')
+
+    publish_static_tf = LaunchConfiguration('publish_static_tf')
+    publish_static_tf_arg = DeclareLaunchArgument('publish_static_tf', default_value='True')
+
+    robot_frame = LaunchConfiguration('robot_frame')
+    robot_frame_arg = DeclareLaunchArgument('robot_frame', default_value='base_footprint')
+
+    lidar_frame = LaunchConfiguration('lidar_frame')
+    lidar_frame_arg = DeclareLaunchArgument('lidar_frame', default_value='velodyne')
+
+    lidar_pose_x = LaunchConfiguration('tf_x')
+    lidar_pose_x_arg = DeclareLaunchArgument('tf_x', default_value='0')
+
+    lidar_pose_y = LaunchConfiguration('tf_y')
+    lidar_pose_y_arg = DeclareLaunchArgument('tf_y', default_value='0')
+
+    lidar_pose_z = LaunchConfiguration('tf_z')
+    lidar_pose_z_arg = DeclareLaunchArgument('tf_z', default_value='0')
+
+    lidar_pose_yaw = LaunchConfiguration('tf_yaw')
+    lidar_pose_yaw_arg = DeclareLaunchArgument('tf_yaw', default_value='0')
 
     #####################################
     # Path settings
@@ -77,10 +98,36 @@ def generate_launch_description():
             laserscan_node    # Laser scan node (point cloud -> laser scan)
         ]
     )
+
+    # Static transform publisher from base_footprint to velodyne
+    laser_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=[
+            # x, y, z, qx, qy, qz, qw
+            lidar_pose_x, lidar_pose_y, lidar_pose_z, 
+            lidar_pose_yaw, '0', '0',
+            # parent_frame, child_frame
+            robot_frame, lidar_frame
+        ],
+        parameters=[{'use_sim_time': is_sim}],
+        output='screen',
+        condition=IfCondition(publish_static_tf)
+    )
     
     #####################################
     # Launch description
     return LaunchDescription([
-        ns_arg, is_sim_arg,
+
+        # --- Arguments ---
+        # Namespace, Simulation, Static TF Publisher
+        ns_arg, is_sim_arg, publish_static_tf_arg,
+        # LiDAR frame, Robot frame
+        lidar_frame_arg, robot_frame_arg,
+        # LiDAR pose (x, y, z, yaw)
+        lidar_pose_x_arg, lidar_pose_y_arg, lidar_pose_z_arg, lidar_pose_yaw_arg,
+
+        # --- Nodes ---
+        laser_tf,
         driver_with_namespace
     ])
