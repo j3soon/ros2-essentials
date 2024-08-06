c136b591211b6f990ecdc4175fb7684afe73904b
Assume-Zhan <assume0701@gapp.nthu.edu.tw>
2024-08-06 22:11:41 +0800

    fix(vlplidar_ws): Remove unused part in in bringup script

--- vlplidar_ws/scripts/lidar-slam-bringup.sh
+++ vlplidar_ws/scripts/lidar-slam-bringup.sh
@@ -4,10 +4,10 @@ source $ROS2_WS/install/setup.bash
 
 {
     ros2 launch vlp_cartographer vlp_driver.launch.py
-} <&0 &
+} &
 
 {
     ros2 launch vlp_cartographer cartographer_demo.launch.py using_odom:=False
-} <&0
+}
 
 wait
