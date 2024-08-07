de4f212efe17b57897a3f5108e270e1ae3ef43db
Assume-Zhan <assume0701@gapp.nthu.edu.tw>
2024-08-07 21:08:03 +0800

    fix(vlplidar_ws): Simplified the lidar slam bringup script

--- vlplidar_ws/scripts/lidar-slam-bringup.sh
+++ vlplidar_ws/scripts/lidar-slam-bringup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -e
 
 source $ROS2_WS/install/setup.bash
-
-{
-    ros2 launch vlp_cartographer vlp_driver.launch.py
-} &
-
-{
-    ros2 launch vlp_cartographer cartographer_demo.launch.py using_odom:=False
-}
-
-wait
+ros2 launch vlp_cartographer cartographer_demo.launch.py using_odom:=False
