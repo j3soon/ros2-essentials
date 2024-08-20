afdb58c658c208b7daaa6e512c8a209b7fe99961
Johnson Sun <j3.soon777@gmail.com>
2024-08-21 02:21:28 +0800

    feat(template_ws): Perform `apt-get update` and `rosdep update` in `.bashrc`, Remove redundant `postCreateCommand.sh`

--- template_ws/.devcontainer/postCreateCommand.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update
-sudo rosdep update
-# Note: The following commands are commented out to prevent unintended install/builds.
-# sudo rosdep install --from-paths src --ignore-src -y
-# sudo chown -R user /home/ros2-essentials/
-# colcon build
