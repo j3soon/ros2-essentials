ef8f44c4e842d40df8d346548287a576d64a5ce4
Johnson Sun <j3.soon777@gmail.com>
2024-10-22 18:34:49 +0800

    feat: Add script for pulling all docker images

---
+++ scripts/pull_amd64_docker_images.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# Ref: https://serverfault.com/a/160961
+trap "echo Terminating...; exit;" SIGINT SIGTERM
+
+images=(
+    "osrf/ros:humble-desktop-full"
+    "osrf/ros:noetic-desktop-full"
+    "j3soon/ros2-template-ws"
+    "j3soon/ros2-orbslam3-ws"
+    "j3soon/ros2-rtabmap-ws"
+    "j3soon/ros2-ros1-bridge-ws"
+    "j3soon/ros2-ros1-bridge-build-ws"
+    "j3soon/ros2-cartographer-ws"
+    "j3soon/ros2-husky-ws"
+    "j3soon/ros2-kobuki-ws"
+    "j3soon/ros2-vlp-ws"
+    "j3soon/ros2-gazebo-world-ws"
+    "j3soon/ros2-aloha-ws"
+)
+
+# Loop through each image and pull it
+for image in "${images[@]}"
+do
+    echo "Pulling amd64 Docker image: $image"
+    docker pull --platform=linux/amd64 "$image"
+done
+
+echo "Done."
---
+++ scripts/pull_arm64_docker_images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# Ref: https://serverfault.com/a/160961
+trap "echo Terminating...; exit;" SIGINT SIGTERM
+
+images=(
+    "ros:humble"
+    "ros:noetic"
+    "j3soon/ros2-template-ws"
+    "j3soon/ros2-ros1-bridge-ws"
+    "j3soon/ros2-ros1-bridge-build-ws"
+    "j3soon/ros2-cartographer-ws"
+    "j3soon/ros2-husky-ws"
+    "j3soon/ros2-kobuki-ws"
+    "j3soon/ros2-vlp-ws"
+    "j3soon/ros2-aloha-ws"
+)
+
+# Loop through each image and pull it
+for image in "${images[@]}"
+do
+    echo "Pulling arm64 Docker image: $image"
+    docker pull --platform=linux/arm64 "$image"
+done
+
+echo "Done."
