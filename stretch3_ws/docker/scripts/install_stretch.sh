cc12992d25644dad470bbf8d7af87d73829da599
Justin <justinshih@gapp.nthu.edu.tw>
2025-12-29 05:56:20 +0800

    feat: better docker environment - add .bashrc overwrite for building and quick source libraries - move ros2 packages install into dockerfile - disable install_stretch.sh install in dockerfile, saved the file for future develop - fix bug in compose.yaml, correct env variable name 'ROS2_WS'

--- stretch3_ws/docker/scripts/install_stretch.sh
+++ stretch3_ws/docker/scripts/install_stretch.sh
@@ -23,24 +23,7 @@ pip3 install pyaudio
 echo "Installing Stretch Body..."
 pip3 install hello-robot-stretch-body
 
-# 2. Install System Dependencies for Stretch
-# Common packages required for Stretch navigation and control
-sudo apt-get update && sudo apt-get install -y \
-    python3-opencv \
-    ros-humble-navigation2 \
-    ros-humble-nav2-bringup \
-    ros-humble-slam-toolbox \
-    ros-humble-realsense2-camera \
-    ros-humble-realsense2-description \
-    ros-humble-urdf \
-    ros-humble-xacro \
-    ros-humble-robot-state-publisher \
-    ros-humble-joint-state-publisher \
-    ros-humble-joy \
-    ros-humble-teleop-twist-joy \
-    ros-humble-teleop-twist-keyboard
-
-# 3. Setup Stretch ROS 2 Workspace
+# 2. Setup Stretch ROS 2 Workspace
 # We use the 'workspaces' directory seen in your ls output
 WS_DIR="$HOME/workspaces/stretch_ws"
 echo "Setting up Stretch ROS 2 Workspace at $WS_DIR..."
@@ -52,7 +35,7 @@ git clone -b humble https://github.com/hello-robot/stretch_ros2.git "$WS_DIR/src
 # Clone stretch_install (As requested, for reference/scripts)
 git clone https://github.com/hello-robot/stretch_install.git "$WS_DIR/src/stretch_install"
 
-# 4. Install ROS Dependencies via rosdep
+# 3. Install ROS Dependencies via rosdep
 source /opt/ros/humble/setup.bash
 sudo apt-get update
 rosdep update
