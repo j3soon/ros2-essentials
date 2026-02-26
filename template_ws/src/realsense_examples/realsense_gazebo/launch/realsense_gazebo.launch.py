39da7bdaea5fe79c2ff46126bd567686c90ce17a
CYHsiang <seanchen1117@gmail.com>
2026-02-26 14:15:47 +0800

    refactor(docker_modules/realsense): Merge realsense_description into realsense_gazebo
        - Moved all files from realsense_description to realsense_gazebo
        - Updated CMakeLists.txt, package.xml, launch files to reflect the changes
        - Rename launch files to be more descriptive of their purpose:
            - realsense_gazebo.launch.py for setting up the Gazebo environment with the robot and realsense
            - realsense_bringup.launch.py for bringing up the robot with realsense in Gazebo and RViz

--- template_ws/src/realsense_examples/realsense_gazebo/launch/realsense_gazebo.launch.py
+++ template_ws/src/realsense_examples/realsense_gazebo/launch/realsense_gazebo.launch.py
@@ -3,94 +3,69 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.actions import SetEnvironmentVariable
-from launch.substitutions import PathJoinSubstitution
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    #####################################
-    # Path settings
+    # Declare the robot position argument
+    robot_px = LaunchConfiguration('robot_px')
+    robot_px_arg = DeclareLaunchArgument('robot_px', default_value='0.5')
 
-    # Declared the world path
-    # Ref: https://github.com/ROBOTIS-GIT/turtlebot3_simulations/tree/98a27b20952b11047f454d7ec751f8c742862713/turtlebot3_gazebo/worlds/turtlebot3_world.world
-    world_path = PathJoinSubstitution([
-        FindPackageShare("turtlebot3_gazebo"), 
-        "worlds/turtlebot3_world.world"
+    robot_py = LaunchConfiguration('robot_py')
+    robot_py_arg = DeclareLaunchArgument('robot_py', default_value='0.5')
+
+    robot_pz = LaunchConfiguration('robot_pz')
+    robot_pz_arg = DeclareLaunchArgument('robot_pz', default_value='0.0')
+
+    # Define the path to the model
+    burger_model_path = PathJoinSubstitution([
+        FindPackageShare("realsense_gazebo"), 
+        "models/turtlebot3_burger/model.sdf"
     ])
-
-    # Gazebo launch file path
-    # Ref: https://github.com/ros-simulation/gazebo_ros_pkgs/blob/0f4818cd5d594f044a65e5beaf4a8b296ed00bc6/gazebo_ros/launch/gazebo.launch.py
-    gazebo_path = PathJoinSubstitution([
-        FindPackageShare("gazebo_ros"), 
-        "launch", "gazebo.launch.py"
-    ])
-
-    # Robot description path
-    realsense_description_path = PathJoinSubstitution([
-        FindPackageShare("realsense_description"),
-        "launch", "realsense_description.launch.py"
-    ])
-
-    # Ensure Gazebo classic can resolve model:// URIs used inside model.sdf
-    # (e.g. model://turtlebot3_burger/meshes/d435.dae)
-    realsense_models_dir = os.path.join(
-        get_package_share_directory('realsense_description'),
-        'models'
-    )
-    gazebo_model_path = os.environ.get('GAZEBO_MODEL_PATH', '')
-    if gazebo_model_path:
-        gazebo_model_path = realsense_models_dir + os.pathsep + gazebo_model_path
-    else:
-        gazebo_model_path = realsense_models_dir
-
-    set_gazebo_model_path = SetEnvironmentVariable(
-        name='GAZEBO_MODEL_PATH',
-        value=gazebo_model_path,
-    )
-
-
-    #####################################
-    # Add launch description
-
-    # Launch the robot with realsense
-    # - With given robot position arguments
-    realsense_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(realsense_description_path),
-        launch_arguments={
-            'robot_px': '-2.0',
-            'robot_py': '-0.5',
-            'robot_pz': '0.01'
-        }.items(),
-    )
     
-    # Launch Gazebo with the given world file
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(gazebo_path),
-        launch_arguments={'world': world_path}.items()
+    # Add robot description
+    # Ref: https://github.com/ROBOTIS-GIT/turtlebot3_simulations/blob/98a27b20952b11047f454d7ec751f8c742862713/turtlebot3_gazebo/launch/robot_state_publisher.launch.py
+    urdf_path = os.path.join(
+        get_package_share_directory('realsense_gazebo'),
+        'urdf',
+        'turtlebot3_burger.urdf'
     )
 
-    #####################################
-    # Other nodes
-
-    # RViz
-    rviz_config = PathJoinSubstitution([FindPackageShare("realsense_gazebo"), "rviz", "realsense.rviz"])
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz',
-        output='screen',
-        arguments=['-d', rviz_config]
-    )
-
-    #####################################
-    # Launch description
+    with open(urdf_path, 'r') as infp:
+        robot_desc = infp.read()
     
+    robot_description = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'use_sim_time': True,
+            'robot_description': robot_desc
+        }],
+    )
+
+    # Spawn the robot in Gazebo
+    # Ref: https://github.com/ros-simulation/gazebo_ros_pkgs/blob/d52f7c471d1d2fd9425bf8621290bc4831be8dea/gazebo_ros/launch/spawn_entity_demo.launch.py
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'burger',
+            '-file', burger_model_path,
+            '-x', robot_px,
+            '-y', robot_py,
+            '-z', robot_pz
+        ],
+        parameters=[{'use_sim_time': True}]
+    )
+
     return LaunchDescription([
-        set_gazebo_model_path,
-        gazebo, rviz, realsense_description
+        # Argument
+        robot_pz_arg, robot_px_arg, robot_py_arg,
+
+        # Nodes
+        robot_description, spawn_entity
     ])
