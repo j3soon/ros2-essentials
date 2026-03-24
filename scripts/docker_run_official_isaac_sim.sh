ded73b61494b7d4c39a398d3fa7502011f24f4b0
Johnson Sun <j3.soon777@gmail.com>
2026-03-24 23:53:35 +0800

    feat: Add Isaac Sim 5.1.0, Deprecate Isaac Sim 5.0.0
    - The isaac sim compatibility checker already exists in the binary install (updated docs)
    - The cache path is different with Isaac Sim 5.0.0

--- scripts/docker_run_official_isaac_sim.sh
+++ scripts/docker_run_official_isaac_sim.sh
@@ -8,19 +8,28 @@ cd "$SCRIPT_DIR/.."
 
 # Ref: https://docs.isaacsim.omniverse.nvidia.com/latest/installation/install_container.html#container-deployment
 # Ref: https://github.com/j3soon/docker-isaac-sim
+mkdir -p ~/docker/isaac-sim/cache/main/ov
+mkdir -p ~/docker/isaac-sim/cache/main/warp
+mkdir -p ~/docker/isaac-sim/cache/computecache
+mkdir -p ~/docker/isaac-sim/config
+mkdir -p ~/docker/isaac-sim/data/documents
+mkdir -p ~/docker/isaac-sim/data/Kit
+mkdir -p ~/docker/isaac-sim/logs
+mkdir -p ~/docker/isaac-sim/pkg
+sudo chown -R 1234:1234 ~/docker/isaac-sim
+
 xhost +local:docker
 docker run --name isaac-sim --entrypoint bash -it --gpus all -e "ACCEPT_EULA=Y" --rm --network=host \
   -e "PRIVACY_CONSENT=Y" \
-  -v ~/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
-  -v ~/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
-  -v ~/docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
-  -v ~/docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
-  -v ~/docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
-  -v ~/docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
-  -v ~/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
-  -v ~/docker/isaac-sim/documents:/root/Documents:rw \
+  -v ~/docker/isaac-sim/cache/main:/isaac-sim/.cache:rw \
+  -v ~/docker/isaac-sim/cache/computecache:/isaac-sim/.nv/ComputeCache:rw \
+  -v ~/docker/isaac-sim/logs:/isaac-sim/.nvidia-omniverse/logs:rw \
+  -v ~/docker/isaac-sim/config:/isaac-sim/.nvidia-omniverse/config:rw \
+  -v ~/docker/isaac-sim/data:/isaac-sim/.local/share/ov/data:rw \
+  -v ~/docker/isaac-sim/pkg:/isaac-sim/.local/share/ov/pkg:rw \
+  -u 1234:1234 \
   -v $(pwd):/home/ros2-essentials \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $HOME/.Xauthority:/root/.Xauthority \
-  nvcr.io/nvidia/isaac-sim:5.0.0
+  nvcr.io/nvidia/isaac-sim:5.1.0
